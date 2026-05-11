6b072e901968705632badedd0b1fdeabad9fb45d
RAKESH40-lgtm <ram19042k@protonmail.com>
2026-05-11 17:11:16 +0530

    getting fundamental strong

--- debounce_throttle.js
+++ debounce_throttle.js
@@ -1,10 +1,19 @@
-function callingLog(){
+function callingLog() {
     console.log("called")
 }
 
+/**
+ * Debouncing delays the execution of a function until the user stops triggering the event for a specified time.
+ * Debounce Use Cases
+
+✅ Search autocomplete
+✅ Form validation
+✅ API calls on typing
+✅ Save draft functionality
+ */
 // function debounce(fn , delay){
 //     let timerId
-    
+
 //     return function(){
 //         clearTimeout(timerId)
 //         timerId =setTimeout(()=>{
@@ -12,17 +21,27 @@ function callingLog(){
 //         },delay)
 //     }
 // }
-function throttle(fn , delay){
-    let flag =true
-    return function(...arg){
-        if(flag){
+
+/**
+ * Throttling ensures a function executes at most once in a specified interval, no matter how many times the event occurs.
+ * Scroll Event
+
+When user scrolls continuously:
+
+You don’t want function firing 100 times/sec
+Instead execute every 300ms
+ */
+function throttle(fn, delay) {
+    let flag = true
+    return function (...arg) {
+        if (flag) {
             fn.apply(this, arg)
-            flag=false
-            setTimeout(()=>{
-                flag=true
-            },delay)
+            flag = false
+            setTimeout(() => {
+                flag = true
+            }, delay)
         }
     }
 }
-let calledDebunce = throttle(callingLog,500)
+let calledDebunce = throttle(callingLog, 500)
 calledDebunce()
---
+++ truthy_falsyVal.js
@@ -0,0 +1,12 @@
+/**
+ * Truthy and falsy values 
+ * Some values considered as truth and falsy 
+ * To check the truthy or falsy can be done by Boolean() or by using !
+ * Truthy values: Number except 0, 
+ * Falsy value : 0 , NAN  ,"" ,undefined , null ,-0
+ * 
+ * typeof
+ */
+console.log(typeof null) //Typeof is object 
+console.log(typeof undefined) // undefined 
+console.log(typeof NaN)
